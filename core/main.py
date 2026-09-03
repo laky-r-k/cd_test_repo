9405101f8fe2b0aa0ad45704184053fbf795362d
laky-r-k <lakshmishrkanchan12@gmail.com>
2026-09-03 17:22:33 +0530

    khjhbb

--- core/main.py
+++ core/main.py
@@ -1,11 +1,12 @@
 import numpy
 import matplotlib.pyplot as plt
+import asdjfjs
 def f(x):
     if x > 0:
         return x
     else:
-        pass
+        return 0
 x = numpy.linspace(0, 10, 100)
-y = f(xasjdf);
+y = f(x)
 
 
